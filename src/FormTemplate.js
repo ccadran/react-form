8cebbc9f715c83b32ac404a7902bdc1da6dad1e3
ccadran <clariocadran33@gmail.com>
2023-03-10 19:58:10 +0100

    add error/success msg

--- src/FormTemplate.js
+++ src/FormTemplate.js
@@ -6,6 +6,7 @@ const FormTemplate = () => {
 
     const sendEmail = (e) => {
         e.preventDefault();
+        const formMess = document.querySelector(".form-message");
 
         emailjs
             .sendForm(
@@ -17,23 +18,38 @@ const FormTemplate = () => {
             .then(
                 (result) => {
                     console.log(result.text);
+                    form.current.reset();
+                    formMess.innerHTML =
+                        "<p class='success'>Message envoyé ! </p>";
+
+                    setTimeout(() => {
+                        formMess.innerHTML = "";
+                    }, 2500);
                 },
                 (error) => {
                     console.log(error.text);
+                    formMess.innerHTML =
+                        "<p class='error'>Une erreur s'est produite, veuillez réessayer </p>";
+                    setTimeout(() => {
+                        formMess.innerHTML = "";
+                    }, 2500);
                 }
             );
     };
 
     return (
-        <form ref={form} onSubmit={sendEmail}>
-            <label>Name</label>
-            <input type="text" name="name" required autoComplete="off" />
-            <label>Email</label>
-            <input type="email" name="email" required autoComplete="off" />
-            <label>Message</label>
-            <textarea name="message" required autoComplete="off" />
-            <input type="submit" value="Send" />
-        </form>
+        <div className="form-container">
+            <form ref={form} onSubmit={sendEmail}>
+                <label>Name</label>
+                <input type="text" name="name" required autoComplete="off" />
+                <label>Email</label>
+                <input type="email" name="email" required autoComplete="off" />
+                <label>Message</label>
+                <textarea name="message" required autoComplete="off" />
+                <input type="submit" value="Send" />
+            </form>
+            <div className="form-message"></div>
+        </div>
     );
 };
 
